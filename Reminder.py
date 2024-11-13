6ed319f22b55ce8ec97702f8de9f176dfe7d3f64
Zeuss Savitski Home <o.zeuss@yandex.ru>
2024-11-13 10:08:01 +0300

    Добавлена функция установки времени

--- Reminder.py
+++ Reminder.py
@@ -6,6 +6,22 @@ import time
 import pygame
 
 
+def set():
+    rem = sd.askstring('Время напоминания', 'введите время напоминания в формате ЧЧ:ММ')
+    if rem:
+        try:
+            hour = int(rem.split(':')[0])
+            minute = int(rem.split(':')[1])
+            now = datetime.now()
+            print(now)
+            dt - now.replace(hour=hour, minute=minute)
+            print(dt)
+            t = dt.timestamp()
+            print(t)
+        except Exception as e:
+            mb.showerror('Ошибка', f'Произошла ошибка {e}')
+
+
 window = Tk()
 window.title('Напоминание')
 label = Label(text='Установите напоминание')
